d0d4dd72e8c9fae6dad75d3355cf55a2fe3a6025
Vinicius Drumond Duarte <viniciusdduarte@outlook.com>
2023-09-05 14:28:34 -0300

    Commit 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,8 +1,23 @@
-// Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
-};
+
+
+export const searchCities = async (term) => {
+  try{
+    const token = d5d8b558f2c741b8945150501230509;
+    const url = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const city = data.find(data => data.name.toLowerCase() === term.toLowerCase());
+  if (!city) {
+    window.alert('Nenhuma cidade encontrada');
+    return [];
+  }
+    return [city];
+  } catch (error) {
+    console.error(error);
+    window.alert('Ocorreu um erro ao buscar a cidade.');
+    return [];
+}};
 
 export const getWeatherByCity = (/* cityURL */) => {
 //   seu código aqui
